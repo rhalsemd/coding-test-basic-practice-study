e75ec98ea6b9290310530e9cf07b197070189758
한승준 <tmdwns950428@naver.com>
2023-06-02 23:36:02 +0900

    20230602 스터디 결과 업로드
    20230602 스터디 결과 업로드

--- 승준/20230602/tile_decoration_13301.js
+++ 승준/20230602/tile_decoration_13301.js
@@ -6,19 +6,19 @@ const rl = readline.createInterface({
 });
 
 rl.on('line', (input) => {
-    const n = parseInt(input);
-    const dp = new Array(n+1);
+  const n = parseInt(input);
+  const dp = new Array(n + 1);
 
-    dp[0] = 4;
-    dp[1] = 6;
+  dp[0] = BigInt(4);
+  dp[1] = BigInt(6);
 
-    for (let i = 2; i < n+1; i++) {
-        dp[i] = dp[i-1] + dp[i-2];
-    }
+  for (let i = 2; i < n + 1; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
+  }
 
-    console.log(dp[n-1]);
+  console.log(dp[n-1].toString());
 
-    rl.close();
+  rl.close();
 }).on("close", () => {
-    process.exit();
+  process.exit();
 });
