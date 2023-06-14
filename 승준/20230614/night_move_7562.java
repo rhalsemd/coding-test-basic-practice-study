0b44866fd5d48125e0589ade1e96b9f1d78ae6e9
한승준 <tmdwns950428@naver.com>
2023-06-15 01:09:47 +0900

    20230614 스터디 결과 업로드
    20230614 스터디 결과 업로드

--- 승준/20230614/night_move_7562.java
+++ 승준/20230614/night_move_7562.java
@@ -1,12 +1,10 @@
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class night_move_7562 {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCases = Integer.parseInt(br.readLine());
@@ -21,13 +19,62 @@ public class night_move_7562 {
             int ty = Integer.parseInt(st.nextToken());
 
             // 최소 움직여서 타겟지점으로 가는 함수
+            int minMoves = getMinMoves(len, cx, cy, tx, ty);
+            System.out.println(minMoves);
         }
     }
 
     public static int getMinMoves(int len, int cx, int cy, int tx, int ty) {
+        int[] dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+        int[] dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+
         boolean[][] visited = new boolean[len][len];
-        // 큐랑 연결리스트 CS이론 확인
         Queue<Point> queue = new LinkedList<>();
-        return 1;
+        queue.offer(new Point(cx, cy));
+        visited[cx][cy] = true;
+
+        int minMoves = 0;
+        boolean foundTarget = false;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Point c = queue.poll();
+
+                if (c.x == tx && c.y == ty) {
+                    foundTarget = true;
+                    break;
+                }
+
+                for (int j = 0; j < 8; j++) {
+                    int nx = c.x + dx[j];
+                    int ny = c.y + dy[j];
+
+                    if (nx >= 0 && nx < len && ny >= 0 && ny < len && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.offer(new Point(nx, ny));
+                    }
+                }
+            }
+
+            if (foundTarget) {
+                break;
+            }
+
+            minMoves++;
+        }
+
+        return minMoves;
+    }
+
+    public static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
